b7b6c8311d552b033e307e7394e8556e06edc141
grosser <grosser.michael@gmail.com>
2010-01-11 19:15:22 +0100

    bugfix

--- lib/tracked_plugins.rb
+++ lib/tracked_plugins.rb
@@ -123,7 +123,7 @@ end
 # always complains when reinstalling, even though there is not externals!
 class RailsEnvironment
   def externals_with_svn_check=(items)
-    externals_without_svn_check=(items) if use_externals?
+    self.externals_without_svn_check=(items) if use_externals?
   end
   alias_method_chain :externals=, :svn_check
 end
